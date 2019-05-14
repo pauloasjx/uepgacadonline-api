b69fb9db6a148901752902ad0691a4b27406b503
Paulo Alves <paulo@esfera.com.br>
2019-05-14 02:34:48 -0300

    Update: Parse Data

--- models/news.py
+++ models/news.py
@@ -13,8 +13,9 @@ class News:
     updated_at: datetime.datetime = None
 
     def __post_init__(self):
-        pass
-        # if type(self.created_at) == str:
-        #     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-        #     self.created_at = datetime.datetime.strptime(self.created_at, '%d %b %Y')
-        #     self.updated_at = datetime.datetime.strptime(self.updated_at, '%d %b %Y')
+        if type(self.created_at) == str:
+            locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+            self.created_at = datetime.datetime.strptime(self.created_at, '%d/%m/%Y - %Hh%M')
+        if type(self.updated_at) == str:
+            locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+            self.updated_at = datetime.datetime.strptime(self.updated_at, '%d/%m/%Y - %Hh%M')
