e4527883915d16926bc21a4da5187ed0bfa7af17
Paulo Alves <juninhpaulo@hotmail.com>
2019-07-28 05:55:24 -0300

    Update: Usando token armazenado no login

--- blueprints/chat_blueprint.py
+++ blueprints/chat_blueprint.py
@@ -4,16 +4,21 @@ from flask_socketio import emit, join_room
 from blueprints import socketio
 from models.user import User
 
+import json
+
 chat_blueprint = Blueprint("chat", __name__)
 
 
 @socketio.on('message', namespace='/chat')
-def message(json):
-    user = User.query.filter_by(token=json['token']).first()
+def message(data):
+    data = json.loads(data)
+
+    user = User.query.filter_by(token=data['token']).first()
 
     if user is not None:
+
         emit('message', {
-            'academic_register': user['academic_register'],
-            'name': user['name'],
-            'message': json['message']
+            'academic_register': user.academic_register,
+            'name': user.name,
+            'message': data['message']
         })
